b33a7ee2bafefedd85034ff87f9787dedf2d66f5
Tal Shachar <tal.shachar@auctionata.com>
2018-01-28 16:36:19 +0200

    add activity

--- app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -3,9 +3,21 @@ package com.mytaxi.android_demo;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
+import com.mytaxi.android_demo.activities.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Rule;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +28,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        Thread.sleep(6000);
+
+        onView(withId(R.id.btn_login))
+                 .perform(click());
+
+
 
         assertEquals("com.mytaxi.android_demo", appContext.getPackageName());
+
     }
 }
